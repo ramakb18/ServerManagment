ba59c1b6b1af4970a04c4b4a2ec4c2e6fa74f38f
RA <RA@PROELE29>
2024-11-19 08:47:10 +0100

    Add project files.

---
+++ ServerManagment/Models/CitiesRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace ServerManagment.Models
+{
+    public static class CitiesRepository
+    {
+        private static List<string> Cities = new List<string>()
+        {
+            "Toronto",
+            "montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+        public static List<string> GetCities() => Cities;
+    }
+}
